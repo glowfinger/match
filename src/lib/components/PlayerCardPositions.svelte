827cece89bd0bcba71cbc3e32dee0ab2f58b5d11
Darren Lilley <darren@glowfinger.com>
2024-12-09 22:06:44 +0000

    Missing bits

---
+++ src/lib/components/PlayerCardPositions.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { SHIRT_NUMBERS } from '$lib/counts/PlayerCounts';
+	import type { Player } from '$lib/IndexedDB';
+
+	type Props = {
+		player: Player;
+	};
+
+	let { player }: Props = $props();
+</script>
+
+<div class="p-2">
+	{#each player.positions.other as position}
+		<span
+			class="mr-2 inline-flex items-center rounded-md bg-slate-50 px-2 py-1 text-xs font-medium text-slate-700 ring-1 ring-inset ring-slate-700/10"
+			>{position}</span
+		>
+	{/each}
+</div>
