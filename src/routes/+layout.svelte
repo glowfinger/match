391fa6293492eb350f17b3695301c7044b51494b
Darren Lilley <darren@glowfinger.com>
2024-12-09 22:49:06 +0000

    Added connection inidcator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
+	import Navbar from '$lib/components/layout/Navbar.svelte';
+	import { network } from '$lib/stores/NetworkStore.svelte';
 	import '../app.css';
+
 	let { children } = $props();
+
+	function handleOnline() {
+		network.online = true;
+	}
+
+	function handleOffline() {
+		network.online = false;
+	}
 </script>
 
+<svelte:window ononline={handleOnline} onoffline={handleOffline} />
+
+<Navbar />
 <div class="flex min-h-full justify-center">
 	<div class=" w-full max-w-sm px-2">
 		{@render children()}
