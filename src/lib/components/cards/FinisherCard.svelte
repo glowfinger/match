cbbc7db0d26124c1c4e884b8359e2129ef863463
Darren Lilley <darren@glowfinger.com>
2024-12-17 19:28:36 +0000

    Added replacements

---
+++ src/lib/components/cards/FinisherCard.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { Player } from '$lib/IndexedDB';
+	import PlayerAvatar from '../avatars/PlayerAvatar.svelte';
+
+	type Props = {
+		player: Player;
+	};
+
+	let { player }: Props = $props();
+</script>
+
+<div
+	class="relative flex items-center space-x-2 rounded-lg border border-slate-300 bg-white p-2 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-slate-400"
+>
+	{#if player && player.images?.length > 0}
+		<div class="shrink-0">
+			<img
+				class="size-10 rounded-full"
+				src={`https://glowfinger.blob.core.windows.net/smg/thumbnails-260x260/${player.images[0].url}.png`}
+				alt=""
+			/>
+		</div>
+	{:else if player}
+		<PlayerAvatar {player} />
+	{:else}
+		<div class="shrink-0">
+			<span
+				class="inline-flex size-10 items-center justify-center rounded-full border border-slate-200 bg-slate-100"
+			>
+				<span class="font-bold text-white"></span>
+			</span>
+		</div>
+	{/if}
+	<div class="min-w-0 flex-1">
+		<span class="absolute inset-0" aria-hidden="true"></span>
+		{#if player}
+			<p class="text-sm font-medium text-slate-900">{player.bio.first} {player.bio.last}</p>
+		{:else}
+			<p class="truncate text-sm text-slate-900">Select player</p>
+		{/if}
+	</div>
+</div>
---
+++ src/lib/components/cards/TeamCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { Team } from '$lib/types/Team';
+
+	type Props = {
+		team: Team;
+	};
+	let { team }: Props = $props();
+</script>
+
+<div
+	class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white p-2 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+>
+	<div class="shrink-0">
+		<img
+			class="size-10 rounded-full"
+			src={`https://glowfinger.blob.core.windows.net/smg/other-badges/${team.badge}`}
+			alt=""
+		/>
+	</div>
+	<div class="min-w-0 flex-1">
+		<a href="#" class="focus:outline-none">
+			<span class="absolute inset-0" aria-hidden="true"></span>
+			<p class="text-sm font-medium text-gray-900">{team.name}</p>
+
+			<p class="truncate text-sm text-gray-500">{team.country}</p>
+		</a>
+	</div>
+</div>
