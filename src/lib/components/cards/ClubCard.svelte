0df74e55d625663121dfe5bdb62d53c18e00e67d
Darren Lilley <darren@glowfinger.com>
2025-04-08 20:24:34 +0100

    Cleaned up

--- src/lib/components/cards/ClubCard.svelte
+++ src/lib/components/cards/ClubCard.svelte
@@ -8,16 +8,16 @@
 </script>
 
 <div class="group relative aspect-5/7 overflow-hidden border border-slate-600 bg-white">
-	<div class=" perspective-distant absolute inset-0 flex items-center justify-center">
+	<div class=" absolute inset-0 flex items-center justify-center perspective-distant">
 		<img
-			class="  group-hover:translate-z-8 group-hover:rotate-x-50 group-hover:rotate-z-45 h-full object-cover transition duration-300 ease-in-out group-hover:scale-150"
+			class="  h-full object-cover transition duration-300 ease-in-out group-hover:translate-z-8 group-hover:scale-150 group-hover:rotate-x-10 group-hover:rotate-z-10"
 			src={club.badge}
 			alt=""
 		/>
 	</div>
 
 	<div
-		class="duration-400 absolute inset-0 flex items-center justify-center transition ease-in-out group-hover:bg-slate-900/50 group-hover:backdrop-blur-xs"
+		class="absolute inset-0 flex items-center justify-center transition duration-400 ease-in-out group-hover:bg-slate-900/50 group-hover:backdrop-blur-xs"
 	>
 		<h2 class="w-full bg-slate-700/60 text-center text-lg font-bold text-white">{club.name}</h2>
 	</div>
