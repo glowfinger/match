afc61d716f77ee080aad4a4fe2c2d4b04d432083
Darren Lilley <glowfinger@users.noreply.github.com>
2026-01-12 23:31:39 +0000

    Social auth (#13)
    * Added working user access
    
    * Added working user access

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../../app.css';
+	let { children } = $props();
+</script>
+
+<div class="flex min-h-full justify-center">
+	<div class="w-full max-w-md min-w-xs p-4">
+		<div class="grid grid-cols-1 gap-2">
+			{@render children()}
+		</div>
+	</div>
+</div>
---
+++ src/routes/api/access/+server.ts
@@ -0,0 +1,11 @@
+import { PRIVATE_ALLOWED_EMAILS } from '$env/static/private';
+import { json } from '@sveltejs/kit';
+export async function POST({ request }) {
+	const { email } = await request.json();
+
+	const allowedEmails = PRIVATE_ALLOWED_EMAILS.split(',').map((email) => email.trim());
+	if (allowedEmails.includes(email)) {
+		return json({ allowed: true }, { status: 200 });
+	}
+	return json({ allowed: false }, { status: 401 });
+}
